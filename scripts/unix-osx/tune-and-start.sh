18b8c09611494e051c32b92abc752bfcc216efc5
Yuri Simione <yuri.simione@artika.biz>
2018-06-16 10:13:25 +0200

    Update tune-and-start.sh

--- scripts/unix-osx/tune-and-start.sh
+++ scripts/unix-osx/tune-and-start.sh
@@ -1,8 +1,22 @@
-export ADOBE_HOME=/Users/yuri/Documents/VMware/ADOBE
-export MAIN_RUN_MODE=author
-export OAK_RUN=$ADOBE_HOME/$MAIN_RUN_MODE/oak-run-1.6.9.jar
+#####################################################
+# Startup script for a local AEM development instance
+#
+ADOBE_HOME=/Users/yuri/Documents/VMware/ADOBE
+MAIN_RUN_MODE=author
+LOCAL_RUN_MODE=yuri-dev 
+OAK_RUN=$ADOBE_HOME/$MAIN_RUN_MODE/oak-run-1.6.9.jar
+HEAP_SPACE=2g 
+JMX_PORT=9999
+DEBUG_PORT=8000
+INSTANCE_PATH=author
+CQ_PORT=4502
+CQ_JARFILE=$ADOBE_HOME/$INSTANCE_PATH/AEM_6.3_Quickstart.jar
+START_OPTS="-p $CQ_PORT -gui"
+PATH=$PATH:$ADOBE_HOME/$INSTANCE_PATH/crx-quickstart/bin #just if you need to run stop or status command from the current shell
+$CQ_JVM_OPTS=-Dcom.sun.management.jmxremote.port=$JMX_PORT -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -server -Xmx$HEAP-SPACE  -Xrunjdwp:transport=dt_socket,address=$DEBUG_PORT,suspend=n,server=y -Dsling.run.modes=\'$MAIN_RUN_MODE,$LOCAL_RUN_MODE\' -Xdebug  
 
-cd $ADOBE_HOME/$MAIN_RUN_MODE
+
+cd $ADOBE_HOME/$INSTANCE_PATH
 
 echo Cleaning and tuning environment before to run the AEM local instance
 echo 1. Removing old log files
@@ -18,7 +32,14 @@ java -jar $OAK_RUN checkpoints install-folder/crx-quickstart/repository/segments
 echo 4. Compacting the SegmentStore (TAR)
 java -jar $OAK_RUN compact install-folder/crx-quickstart/repository/segmentstore 
 
-echo 5. Starting AEM, debug mode, JMX enabled
-java -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -server -Xmx2g  -Xrunjdwp:transport=dt_socket,address=8000,suspend=n,server=y -Dsling.run.modes='author,ysvlocaldev' -Xdebug -jar ./AEM_6.3_Quickstart.jar -p 4502 -gui
+echo 5. Starting $MAIN_RUN_MODE AEM instance and $LOCAL_RUN_MODE run mode, debug enabled on port $DEBUG_PORT, JMX enabled on port $JMX_PORT
 
- 
+(
+  (
+    java $CQ_JVM_OPTS -jar $CQ_JARFILE $START_OPTS &
+    echo $! > $CURR_DIR/crx-quickstart/conf/cq.pid
+  ) >> $CURR_DIR//crx-quickstart/logs/stdout.log 2>&1
+) &
+
+
+
